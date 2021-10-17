c4aa447cff403c3212d094943622464354de966f
dombroks <tinoutech@gmail.com>
2021-10-17 13:33:36 +0100

    - Setting the adapter up with view binding.

--- app/src/main/java/com/example/recyclerviewtemplate_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewtemplate_kotlin/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.recyclerviewtemplate_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.recyclerviewtemplate_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
     }
 }
--- app/src/main/java/com/example/recyclerviewtemplate_kotlin/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerviewtemplate_kotlin/RecyclerAdapter.kt
@@ -1,27 +1,31 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ${NAME} : RecyclerView.Adapter<${Model}Adapter.${ViewHolder_NAME}>() {
 
+    private var binding: ${BINDING_CLASS}? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ${ViewHolder_NAME} {
-        val view = LayoutInflater.from(context).inflate(R.layout.${item_NAME}, parent, false)
-        return ${ViewHolder_NAME}(view)
+
+        binding =
+        ${BINDING_CLASS}.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ${ViewHolder_NAME}(binding!!);
     }
 
     override fun getItemCount() = OJGName.size
 
     override fun onBindViewHolder(holder: ${ViewHolder_NAME}, position: Int) {
-        holder.bind(data[position])
+        with(holder){
+            // Do your binding here.
+        }
     }
 
-    class ${ViewHolder_NAME}(itemView: View) : RecyclerView.${ViewHolder_NAME}(itemView) {
-        fun bind(item: ${Model}) = with(itemView) {
-        // TODO: Bind the data with View
-    }
-    }
+    inner class ${ViewHolder_NAME}(val binding: ${BINDING_CLASS}) :
+        RecyclerView. ${ViewHolder_NAME}(binding.root)
+
+
 }
